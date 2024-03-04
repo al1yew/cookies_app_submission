15b1ca6c46bfdd86ddf1365d73e4930f84b3b8e9
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 12:50:14 +0000

    Mar 4, 2024, 1:50 PM

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,9 +1,35 @@
+"use client";
+
 import Image from "next/image";
 import Container from "./Container";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header
+      className={`w-full z-[99999] fixed top-0 left-0 bg-transparent duration-200 ${
+        isScrolled ? "py-3 bg-opacity-40 bg-white" : "py-4"
+      }`}
+    >
       <Container>
         <div className="flex justify-between items-center">
           <span className="flexCenter sm:hidden px-2">
