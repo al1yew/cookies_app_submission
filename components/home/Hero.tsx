152045ac306e477e2704ebfe3b1bb970eb2acf0c
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 13:07:06 +0000

    Mar 4, 2024, 2:07 PM

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,17 @@
+import Image from "next/image";
+import Container from "../shared/Container";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full relative">
+      <Container>
+        <div className="absolute left-1/2 -top-18 -translate-x-1/2 -z-[1]">
+          <div className="relative">
+            
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
 };
 export default Hero;
