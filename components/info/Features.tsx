aa905cc0714ef6f47e3074f39487f21b88cc24fa
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 20:26:53 +0000

    Mar 4, 2024, 9:26 PM

--- components/info/Features.tsx
+++ components/info/Features.tsx
@@ -1,13 +1,24 @@
 import { FEATURES } from "@/lib/constants";
 import Container from "../shared/Container";
+import Image from "next/image";
 
 const Features = () => {
   return (
     <section className="w-full relative mt-24">
       <Container>
-        <div className="features">
+        <div className="grid grid-cols-1 lg:gap-8 gap-4 lg:grid-cols-4 bg-[#FBFBFB] lg:w-11/12 w-full p-10 rounded-3xl mx-auto">
           {FEATURES.map((feature, i) => {
-            return <div></div>;
+            return (
+              <div className="col-span-1 flex flex-col justify-between">
+                <Image src={feature.icon} width={30} height={30} alt="icon" />
+                <h1 className="font-black lg:text-2xl text-xl mt-3 mb-5">
+                  {feature.title}
+                </h1>
+                <p className="font-medium lg:text-lg text-md text-[#5B5B5B]">
+                  {feature.text}
+                </p>
+              </div>
+            );
           })}
         </div>
       </Container>
