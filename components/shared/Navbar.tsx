7c09755b2616459da0d5c427b9c23f4be5e20889
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 12:47:43 +0000

    Mar 4, 2024, 1:47 PM

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import Container from "./Container";
 
 const Navbar = () => {
   return (
     <header>
       <Container>
-        <div></div>
+        <div className="flex justify-between items-center">
+          <span className="flexCenter sm:hidden px-2">
+            <Image
+              src="/images/menu.svg"
+              width={24}
+              height={24}
+              alt="menu"
+              className="cursor-pointer"
+            />
+            <span className="h-8 w-[0.5px] bg-gray-300 ml-4"></span>
+          </span>
+        </div>
       </Container>
     </header>
   );
