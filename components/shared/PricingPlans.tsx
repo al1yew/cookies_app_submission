229adecaa0ed3ad05ca979c2408f9636a7d3825c
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 22:46:27 +0000

    Mar 4, 2024, 11:46 PM

--- components/shared/PricingPlans.tsx
+++ components/shared/PricingPlans.tsx
@@ -8,11 +8,11 @@ const PricingPlans = ({ isHomePage }: { isHomePage?: boolean }) => {
   return (
     <section className="w-full relative mt-24 lg:mb-24">
       <div className="max-w-[1320px] mx-auto px-0 lg:px-10 xl:px-15">
-        <div className="bg-black w-full lg:h-[550px] lg:w-10/12 lg:max-w-[900px] lg:rounded-3xl p-3 py-7 pb-3 lg:p-10 mx-auto relative ">
-          <h2 className="font-normal text-md text-[#DBD1D1] text-center tracking-widest uppercase">
+        <div className="bg-black w-full lg:h-[550px] lg:w-11/12 lg:max-w-[1000px] lg:rounded-3xl p-3 py-7 pb-3 lg:p-10 mx-auto relative ">
+          <h2 className="font-normal text-md text-[#DBD1D1] text-center tracking-8 uppercase">
             Tailored solutions for every compliance requirement
           </h2>
-          <h1 className="lg:text-5xl text-3xl font-black text-center mt-2 lg:mt-0 mb-8 lg:mb-4 text-white">
+          <h1 className="lg:text-5xl text-3xl font-black text-center mt-3 lg:mt-5 mb-8 lg:mb-4 text-white">
             Find Your Perfect Plan
           </h1>
           {isHomePage && (
