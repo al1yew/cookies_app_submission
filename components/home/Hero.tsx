cbc8026762733ddba8db4962185dcac4f0c1de3b
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 13:08:02 +0000

    Mar 4, 2024, 2:08 PM

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -7,7 +7,20 @@ const Hero = () => {
       <Container>
         <div className="absolute left-1/2 -top-18 -translate-x-1/2 -z-[1]">
           <div className="relative">
-            
+            <Image
+              src="/images/redCloud.svg"
+              width={171}
+              height={171}
+              alt="icon"
+              className="z-10"
+            />
+            <Image
+              src="/images/tick.svg"
+              width={75}
+              height={75}
+              alt="tick"
+              className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-20"
+            />
           </div>
         </div>
       </Container>
