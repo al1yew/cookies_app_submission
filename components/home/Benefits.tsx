30b16b1724690ef3bc0fec84ba10db970b957ae4
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 14:02:04 +0000

    Mar 4, 2024, 3:02 PM

--- components/home/Benefits.tsx
+++ components/home/Benefits.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Container from "../shared/Container";
 
 const Benefits = () => {
@@ -17,7 +18,41 @@ const Benefits = () => {
             31 41 23 percent
           </div>
           <div className="flex justify-between flex-wrap items-center w-full gap-5">
-            56 41 percent
+            <div className="w-full lg:w-[56%] h-[210px] bg-[#FBFBFB] rounded-2xl p-8 relative overflow-hidden border border-gray-100">
+              <h2 className="font-black text-2xl mb-3">
+                Real-time Cookie Scanning
+              </h2>
+              <p className="font-medium text-md text-[#110000] max-w-[320px] text-wrap">
+                Instantly identify and categorize cookies.
+                <br />
+                Our real-time scanning ensures your website is always compliant.
+              </p>
+              <Image
+                src="/images/circles.svg"
+                width={400}
+                height={400}
+                alt="ring"
+                className="absolute bottom-0 right-0 hidden lg:block"
+              />
+            </div>
+            <div className="w-full lg:w-[41%] h-[210px] bg-black rounded-2xl p-8 relative overflow-hidden border border-gray-100 flex justify-between items-center gap-10">
+              <Image
+                src="/images/rotatedsmile.svg"
+                width={110}
+                height={110}
+                alt="ring"
+                className="hidden lg:block"
+              />
+              <div className="flex flex-col items-start justify-between">
+                <h2 className="font-black text-2xl mb-3 text-white">
+                  User Experience Optimization
+                </h2>
+                <p className="font-medium text-md text-[#7B7B7B]">
+                  Enhance visitor satisfaction. Balance compliance with an
+                  optimal user experience on your website.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
