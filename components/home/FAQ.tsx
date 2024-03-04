8c852e082444e1259a532cb4c430bb41d0dceea6
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 19:05:29 +0000

    Mar 4, 2024, 8:05 PM

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -1,4 +1,19 @@
+import Container from "../shared/Container";
+
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <section className="w-full relative mt-20 lg:mt-32">
+      <Container>
+        <div className="bg-[#FBFBFB] rounded-3xl p-3 lg:px-32 lg:py-16 lg:w-11/12 lg:max-w-[1100px] w-full mx-auto">
+          <h1 className="text-3xl font-black text-center lg:text-start">
+            Frequently Asked Questions
+          </h1>
+          <div className="w-full flex-col flex gap-5 lg:gap-8 mt-10">
+            questions goes here
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
 };
 export default FAQ;
--- components/home/Feedback.tsx
+++ components/home/Feedback.tsx
@@ -5,13 +5,20 @@ const Feedback = () => {
   return (
     <section className="w-full relative mt-32">
       <Container>
-        <div className="lg:w-10/12 lg:max-w-[1000px] w-full mx-auto flex items-center justify-between flex-wrap gap-6 lg:gap-32">
+        <div className="lg:w-10/12 lg:max-w-[900px] w-full mx-auto flex items-center justify-between flex-wrap gap-6 lg:gap-32">
           <div className="lg:flex-1">
-            dsadasdsa Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolor, cupiditate voluptatum ducimus beatae obcaecati ut voluptates
-            assumenda mollitia excepturi similique quia ex? Error minus
-            veritatis itaque fugiat voluptatum aspernatur consequatur in qui
-            fuga ipsam, atque, et quos reiciendis nobis mollitia.
+            <p className="italic font-medium text-xl text-black">
+              Aliquam tristique sed vulputate amet. Amet a orci nunc risus
+              scelerisque varius placerat bibendum tristique. Ut pharetra non eu
+              in cras ullamcorper viverra ac malesuada. Venenatis proin non
+              magna ullamcorper nec adipiscing condimentum.
+            </p>
+            <h1 className="capitalize mt-6 lg:mt-10 font-black text-[44px]">
+              John Doe
+            </h1>
+            <h3 className="uppercase text-[#7B6969] text-md font-normal tracking-widest">
+              Lorem Ipsum Company
+            </h3>
           </div>
 
           <div className="lg:flex-1">
