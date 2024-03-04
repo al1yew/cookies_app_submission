e33207c5577aaa17ecf0d44759965ae553c12ce8
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 19:16:26 +0000

    Mar 4, 2024, 8:16 PM

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -2,9 +2,17 @@
 
 import { QUESTIONS } from "@/lib/constants";
 import Container from "../shared/Container";
+import { useState } from "react";
 import SingleQuestion from "./SingleQuestion";
 
 const FAQ = () => {
+  const [activeId, setActiveId] = useState<number>(1);
+
+  const toggleQuestion = (id: number) => {
+    const newActiveId = id === activeId ? 0 : id;
+    setActiveId(newActiveId);
+  };
+
   return (
     <section className="w-full relative mt-20 lg:mt-32">
       <Container>
@@ -14,7 +22,14 @@ const FAQ = () => {
           </h1>
           <div className="w-full flex-col flex gap-5 lg:gap-8 mt-10">
             {QUESTIONS.map((question) => {
-              return <SingleQuestion key={question.id} {...question} />;
+              return (
+                <SingleQuestion
+                  key={question.id}
+                  {...question}
+                  activeId={activeId}
+                  toggleQuestion={toggleQuestion}
+                />
+              );
             })}
           </div>
         </div>
--- components/home/SingleQuestion.tsx
+++ components/home/SingleQuestion.tsx
@@ -1,13 +1,43 @@
+import Image from "next/image";
+
 type Props = {
+  id: number;
   question: string;
   answer: string;
+  activeId: number;
+  toggleQuestion: (id: number) => void;
 };
 
-const SingleQuestion = ({ question, answer }: Props) => {
+const SingleQuestion = ({
+  id,
+  question,
+  answer,
+  activeId,
+  toggleQuestion,
+}: Props) => {
+  const isActive = id === activeId;
+
   return (
     <div className="bg-white p-4 lg:p-7 relative w-full rounded-xl">
-      <h3 className="font-black text-2xl">{question}</h3>
-      <div>{answer}</div>
+      <h3 className="font-black text-2xl max-w-[80%] lg:max-w-full">
+        {question}
+      </h3>
+      <div
+        className={`max-w-[80%] duration-200 overflow-hidden transition-height ${
+          isActive ? "h-auto mt-6" : "h-0"
+        }`}
+      >
+        {answer}
+      </div>
+      <span className="absolute top-3 lg:top-1/2 lg:-translate-y-1/2 lg:right-5 right-3 cursor-pointer">
+        <Image
+          src={isActive ? "/images/minus.svg" : "/images/plus.svg"}
+          height={27}
+          width={27}
+          alt="close question"
+          onClick={() => toggleQuestion(id)}
+        />
+      </span>
     </div>
   );
 };
