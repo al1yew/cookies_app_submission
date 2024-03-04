886c43242faadadb424372c9e8cd1106105934b3
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 20:53:25 +0000

    Mar 4, 2024, 9:53 PM

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -4,6 +4,7 @@ import { QUESTIONS } from "@/lib/constants";
 import Container from "../shared/Container";
 import { useState } from "react";
 import SingleQuestion from "./SingleQuestion";
+import Image from "next/image";
 
 const FAQ = () => {
   const [activeId, setActiveId] = useState<number>(1);
@@ -16,7 +17,14 @@ const FAQ = () => {
   return (
     <section className="w-full relative mt-20 lg:mt-32">
       <Container>
-        <div className="bg-[#FBFBFB] rounded-3xl p-3 lg:px-32 lg:py-16 lg:w-11/12 lg:max-w-[1100px] w-full mx-auto">
+        <div className="bg-[#FBFBFB] rounded-3xl p-3 lg:px-28 lg:py-16 lg:w-10/12 lg:max-w-[1100px] w-full mx-auto">
+          <Image
+            src="/images/flower.svg"
+            width={400}
+            height={400}
+            alt="squares"
+            className="absolute -left-7 -top-44 hidden lg:block -z-10"
+          />
           <h1 className="text-3xl font-black text-center lg:text-start">
             Frequently Asked Questions
           </h1>
