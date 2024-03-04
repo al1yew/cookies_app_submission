ad2d793a9c4b6e65d4ceaf0afc5ed5cb4c55cc9e
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 14:12:13 +0000

    Mar 4, 2024, 3:12 PM

--- components/home/Management.tsx
+++ components/home/Management.tsx
@@ -1,4 +1,27 @@
+import Image from "next/image";
+import Container from "../shared/Container";
+
 const Management = () => {
-  return <div>Management</div>;
+  return (
+    <section className="w-full relative mt-24">
+      <Container>
+        <div>
+          <Image
+            src="/images/cells2.svg"
+            width={900}
+            height={900}
+            alt="cells"
+            className="hidden lg:block absolute opacity-40 top-40 -left-32 w-2/3  z-[-2]"
+          />
+          <h2 className="text-[#7B6969] font-normal text-md tracking-widest text-center">
+            COOKIE MANAGEMENT
+          </h2>
+          <h1 className="font-black text-[32px] lg:text-[44px] text-center">
+            Your Path to Easy Compliance
+          </h1>
+        </div>
+      </Container>
+    </section>
+  );
 };
 export default Management;
