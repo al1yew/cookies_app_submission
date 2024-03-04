fdb7f80ae96972a2f6b84c58133faa162edb4d47
al1yew <vasifaliyev.baku@gmail.com>
2024-03-04 21:17:18 +0000

    Mar 4, 2024, 10:17 PM

--- app/(noDefaultLayout)/login/page.tsx
+++ app/(noDefaultLayout)/login/page.tsx
@@ -5,39 +5,49 @@ import Link from "next/link";
 const Login = () => {
   return (
     <section className="w-full relative h-screen flex justify-center items-center">
-      <Container>
-        <div className="flex w-full justify-center items-center h-screen ">
-          <div className="bg-[#A5A5A5] lg:flex-1 h-full py-5">
-            <div className="logo">
-              <Link
-                href="/"
-                className="flexStart [&>*:first-child]:hover:rotate-180"
-              >
-                <Image
-                  src="/images/logo.svg"
-                  alt="Cookies Logo"
-                  width={27}
-                  height={27}
-                  className="transition-transform hover:rotate-180"
-                />
-                <span className="ml-4 font-black text-2xl">Cookie Service</span>
-              </Link>
-            </div>
-            <h1 className="font-black lg:text-[56px] text-2xl">Login</h1>
-            <form action="">
-              <input type="text" />
+      <div className="flex w-full justify-center items-center h-screen flex-wrap">
+        <div className="bg-gray-100 lg:flex-1 h-full py-5 flex flex-col justify-between px-4 lg:pl-20 lg:pr-8 xl:pl-32 xl:pr-16">
+          <Link
+            href="/"
+            className="flexStart w-fit [&>*:first-child]:hover:rotate-180"
+          >
+            <Image
+              src="/images/logo.svg"
+              alt="Cookies Logo"
+              width={27}
+              height={27}
+              className="transition-transform hover:rotate-180"
+            />
+            <span className="ml-4 font-black text-2xl">Cookie Service</span>
+          </Link>
+          <div>
+            <h1 className="font-black lg:text-[56px] text-2xl mb-10">Login</h1>
+            <form
+              action=""
+              className="rounded-3xl bg-gray-100 flex flex-col justify-between items-start p-10 shadow-xl"
+            >
+              <input
+                type="text"
+                className="p-3 bg-white text-gray-100 italic border border-gray-100 rounded-2xl"
+                placeholder="Email"
+              />
               <input type="password" name="" id="" />
               <button>Login arrow</button>
             </form>
             <div>do not have account? reg</div>
           </div>
-          <div className="right hidden lg:flex-1 lg:flex lg:flex-col gap-4">
-            <h1 className="font-black text-[40px]">
-              Automated Compliance Updates
-            </h1>
-          </div>
+          <div></div>
         </div>
-      </Container>
+        <div className="right lg:flex-1 flex flex-col gap-4">
+          <h1 className="font-black text-[40px]">
+            Automated Compliance Updates
+          </h1>
+          <p className="font-medium text-xl text-[#110000]">
+            Stay current with evolving laws. Our service automatically aligns
+            your site with the latest cookie regulations.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
