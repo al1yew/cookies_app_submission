8162d11d6fe900f6f05d08161e4217991002563b
al1yew <vasifaliyev.baku@gmail.com>
2024-03-05 02:04:29 +0100

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: [
+    {
+      protocol: "http",
+      hostname: "localhost",
+      port: "3000",
+      pathname: "/images/**",
+    },
+    {
+      protocol: "https",
+      hostname: "<YOUR SITE URL>.com",
+      port: "",
+      pathname: "/images/**",
+    },
+  ],
 };
 
 export default nextConfig;
